f57c296efcc012d12db3abd38b0ee6ec1eb33888
zhurinvlad <vz@poloniumarts.com>
2016-12-29 16:46:36 +0300

    add restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :restaurants
   resources :users
-  root to: "index#index2"
+  root to: "restaurants#index"
   # root to: "index#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161223083801) do
+ActiveRecord::Schema.define(version: 20161229130321) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "addres"
+    t.float    "lat"
+    t.float    "lng"
+    t.string   "image"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
