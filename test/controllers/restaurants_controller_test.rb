606940a1be7f5083988702c51a98fb1b06dc02f8
zhurinvlad <vz@poloniumarts.com>
2017-01-09 13:34:09 +0300

    tmp

--- test/controllers/restaurants_controller_test.rb
+++ test/controllers/restaurants_controller_test.rb
@@ -15,15 +15,15 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should create restaurant" do
+  test "should create restaurants" do
     assert_difference('Restaurant.count') do
-      post restaurants_url, params: { restaurant: { addres: @restaurant.addres, description: @restaurant.description, image: @restaurant.image, lat: @restaurant.lat, lng: @restaurant.lng, name: @restaurant.name } }
+      post restaurants_url, params: {restaurants: {addres: @restaurant.addres, description: @restaurant.description, image: @restaurant.image, lat: @restaurant.lat, lng: @restaurant.lng, name: @restaurant.name } }
     end
 
     assert_redirected_to restaurant_url(Restaurant.last)
   end
 
-  test "should show restaurant" do
+  test "should show restaurants" do
     get restaurant_url(@restaurant)
     assert_response :success
   end
@@ -33,12 +33,12 @@ class RestaurantsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should update restaurant" do
-    patch restaurant_url(@restaurant), params: { restaurant: { addres: @restaurant.addres, description: @restaurant.description, image: @restaurant.image, lat: @restaurant.lat, lng: @restaurant.lng, name: @restaurant.name } }
+  test "should update restaurants" do
+    patch restaurant_url(@restaurant), params: {restaurants: {addres: @restaurant.addres, description: @restaurant.description, image: @restaurant.image, lat: @restaurant.lat, lng: @restaurant.lng, name: @restaurant.name } }
     assert_redirected_to restaurant_url(@restaurant)
   end
 
-  test "should destroy restaurant" do
+  test "should destroy restaurants" do
     assert_difference('Restaurant.count', -1) do
       delete restaurant_url(@restaurant)
     end
